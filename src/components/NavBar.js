ae601132c7f93933d00e68466b8055c68c66dc89
Ray Wang <raywang0@gmail.com>
2020-12-09 18:29:33 -0500

    Added NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
+
+export default function NavBar() {
+  return (
+    <header className="bg-blue-900">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink
+            to="/"
+            exact
+            activeClassName="text-white"
+            className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-purple-300 text-4xl font-bold cursive tracking-widest"
+          >
+            Ray
+          </NavLink>
+          <NavLink
+            to="/post"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-purple-300"
+            activeClassName="text-blue-100 bg-blue-700"
+          >
+            Blog Posts
+          </NavLink>
+          <NavLink
+            to="/project"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-purple-300"
+            activeClassName="text-blue-100 bg-blue-700"
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to="/about"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-purple-300"
+            activeClassName="text-blue-100 bg-blue-700"
+          >
+            About Me!
+          </NavLink>
+        </nav>
+        <div className="inline-flex py-3 px-3 my-6">
+          <SocialIcon
+            url="https://www.linkedin.com/in/rayxw"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://twitter.com/Ray0Scythe"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://www.facebook.com/Scythe.Ray"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+        </div>
+      </div>
+    </header>
+  );
+}
